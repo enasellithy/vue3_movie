ed58ceade5ebc53e4d78b7ca8c39ff5132dc6837
enas ellithy <17440978+enasellithy@users.noreply.github.com>
2022-06-15 22:15:30 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
 		}
   },
-   publicPath: process.env.NODE_ENV === 'production'
-    ? '/' + process.env.CI_PROJECT_NAME + '/vue3_movie'
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/' + process.env.CI_PROJECT_NAME + '/'
     : '/'
 })
