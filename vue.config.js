4ffd51d6d37da41d800fa5735aceaca2578f04f2
enas ellithy <17440978+enasellithy@users.noreply.github.com>
2022-07-12 00:32:00 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,17 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true,
 
-  pluginOptions: {
-    vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
-  },
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/' + process.env.CI_PROJECT_NAME + '/vue3_movie'
-    : '/'
-})
+//   pluginOptions: {
+//     vuetify: {
+// 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+// 		}
+//   },
+//     publicPath: process.env.NODE_ENV === 'production'
+//     ? '/' + process.env.CI_PROJECT_NAME + '/vue3_movie'
+//     : '/'
+// })
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/vue3_movie/" : "/",
+};
